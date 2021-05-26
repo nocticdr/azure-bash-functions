a067d27a4c39bad74c6c997839bacdb3e87687b7
Ked Mardemootoo <ked.m@qigroup.com>
2021-05-27 01:09:57 +0800

    rename file

---
+++ get-azure-agw-state.sh
@@ -0,0 +1,26 @@
+#####################################
+############# FUNCTIONS #############
+#####################################
+# Bash Colours
+#https://gist.github.com/vratiu/9780109
+#https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux?rq=1
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+BOLD='\e[1m'
+UNDERLINE='\e[4m'
+NU='\e[24m' # No Underline
+DGREY='\033[1;30m'
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
